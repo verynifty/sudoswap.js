d96a887bf45ad3d4e171c73b73f4294fe306411d
AH <surfcoderepeat@protonmail.com>
2022-08-28 22:21:25 -0600

    all held ids

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-const SudoSwap = require('./src/SudoSwap.js');
+const SudoSwap = require("./src/SudoSwap.js");
 
 (async () => {
-    try {
+  try {
+    const sudo = new SudoSwap(
+      "https://eth-mainnet.g.alchemy.com/v2/_vDB38mJZ39GyrIYbhoFtEkxYVydis-o"
+    );
+    const pool = sudo.getPool("0x6210e6229aec95d17f57dab93e042013d7d3603c");
 
-        const sudo = new SudoSwap("https://eth-mainnet.g.alchemy.com/v2/_vDB38mJZ39GyrIYbhoFtEkxYVydis-o")
-        const pool = sudo.getPool("0x6210e6229aec95d17f57dab93e042013d7d3603c")
+    let nft = await pool.getNFT();
+    console.log(nft);
 
-        let nft = await pool.getNFT()
-        console.log(nft)
+    let spotPrice = await pool.getSpotPrice();
+    console.log(spotPrice);
 
-        let spotPrice = await pool.getSpotPrice()
-        console.log(spotPrice)
+    let trades = await pool.getTrades();
+    console.log(trades);
+    // return;
+    let type = await pool.getType();
+    console.log(type);
 
-        let trades = await pool.getTrades();
-        console.log(trades);
-    return;
-        let type = await pool.getType()
-        console.log(type)
+    let buyQuote = await pool.getBuyNFTQuote(1);
+    console.log(buyQuote);
 
-        let buyQuote = await pool.getBuyNFTQuote(1);
-        console.log(buyQuote)
+    let sellQuote = await pool.getSellNFTQuote(1);
+    console.log(sellQuote);
 
-        let sellQuote = await pool.getSellNFTQuote(1);
-        console.log(sellQuote)
-    } catch (e) {
-        console.log(e)
-    }
+    let heldIds = await pool.getAllHeldIds();
+
+    console.log(heldIds);
+  } catch (e) {
+    console.log(e);
+  }
 })();
