3a6de701dac33b782d6f46a391361f108b85f98e
grandsmarquis <jdourlens@gmail.com>
2022-08-29 18:01:49 +0900

    address map

--- src/Router.js
+++ src/Router.js
@@ -4,14 +4,13 @@ const NFTABI = require("./ABIS/erc721.json");
 const { ethers } = require("ethers");
 const abiDecoder = require("abi-decoder");
 
+const ADDRESSES = {
+  1: "0x2b2e8cda09bba9660dca5cb6233787738ad68329", // mainnet
+  4: "0x9ABDe410D7BA62fA11EF37984c0Faf2782FE39B5" // rinkeby
+}
 function Router(sudo, chainId) {
   this.sudo = sudo;
-  this.address =
-    chainId == 1
-      ? "0x2b2e8cda09bba9660dca5cb6233787738ad68329" //mainnet
-      : chainId == 4
-      ? "0x9ABDe410D7BA62fA11EF37984c0Faf2782FE39B5" //rinkeby
-      : null;
+  this.address =ADDRESSES[chainId];
 
   this.contract = new ethers.Contract(this.address, ABI, this.sudo.provider);
 }
