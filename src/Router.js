845c06a1b6ccd6528c71b447d7d1914781d33248
grandsmarquis <jdourlens@gmail.com>
2022-08-30 10:18:45 +0900

    rename

---
+++ src/Router.js
@@ -0,0 +1,77 @@
+const ABI = require("./ABIS/router.json");
+const NFTABI = require("./ABIS/erc721.json");
+
+const { ethers } = require("ethers");
+const abiDecoder = require("abi-decoder");
+
+const ADDRESSES = {
+  1: "0x2b2e8cda09bba9660dca5cb6233787738ad68329", // mainnet
+  4: "0x9ABDe410D7BA62fA11EF37984c0Faf2782FE39B5", // rinkeby
+};
+function Router(sudo, chainId = 1) {
+  this.sudo = sudo;
+  this.address = ADDRESSES[chainId];
+  this.chainId = chainId;
+  this.contract = new ethers.Contract(this.address, ABI, this.sudo.provider);
+}
+
+// todo add logic for connecting to signer
+Router.prototype.swapETHForAnyNFTs = async function (
+  swapList,
+  ethRecipient,
+  nftRecipient,
+  deadline,
+  ethAmount
+) {
+  const tx = await this.contract
+    .connect(this.sudo.signer)
+    .swapETHForAnyNFTs(swapList, ethRecipient, nftRecipient, deadline, {
+      value: ethAmount,
+    });
+
+  return tx;
+};
+
+Router.prototype.swapETHForSpecificNFTs = async function (
+  swapList,
+  ethRecipient,
+  nftRecipient,
+  deadline,
+  ethAmount
+) {
+  const tx = await this.contract
+    .connect(this.sudo.signer)
+    .swapETHForSpecificNFTs(swapList, ethRecipient, nftRecipient, deadline, {
+      value: ethAmount,
+    });
+
+  return tx;
+};
+
+Router.prototype.swapNFTsForToken = async function (
+  swapList,
+  minOutput,
+  tokenRecipient,
+  deadline
+) {
+  const tx = await this.contract
+    .connect(this.sudo.signer)
+    .swapNFTsForToken(swapList, minOutput, tokenRecipient, deadline);
+
+  return tx;
+};
+
+Router.prototype.isApprovedForRouter = async function (nftCollection) {
+  let nftContract = new ethers.Contract(
+    nftCollection,
+    NFTABI,
+    this.sudo.provider
+  );
+  let res = await nftContract.isApprovedForAll(
+    this.sudo.signer.address,
+    ADDRESSES[this.chainId]
+  );
+  console.log(res);
+};
+
+module.exports = Router;
---
+++ src/Sudoswap.js
@@ -0,0 +1,73 @@
+const { ethers } = require("ethers");
+const moize = require("moize");
+
+const Pool = require("./Pool");
+const Router = require("./Router");
+
+const BLOCK_CACHE_SIZE = 300;
+const TRANSACTION_CACHE_SIZE = 100;
+
+function Sudoswap(web3Provider, pKey = null) {
+  if (typeof web3Provider == "string") {
+    this.provider = new ethers.providers.JsonRpcProvider(web3Provider);
+  } else {
+    this.provider = new ethers.providers.Web3Provider(web3Provider);
+    this.signer = this.provider.getSigner();
+  }
+
+  // pass pkey for buy and sell functionality
+  if (pKey) {
+    const wallet = new ethers.Wallet(pKey);
+    this.signer = wallet.connect(this.provider);
+  }
+
+  const getBlock = async function (blockNumber) {
+    let b = await this.provider.getBlock(blockNumber);
+    return b;
+  };
+
+  this.getBlock = moize(getBlock, {
+    maxSize: BLOCK_CACHE_SIZE,
+    maxArgs: 1,
+  });
+
+  const getTransaction = async function (blockNumber) {
+    let t = await this.provider.getTransaction(blockNumber);
+    return t;
+  };
+
+  this.getTransaction = moize(getTransaction, {
+    maxSize: TRANSACTION_CACHE_SIZE,
+    maxArgs: 1,
+  });
+}
+
+/*
+    Instanciate a new pool
+*/
+Sudoswap.prototype.getPool = function (address) {
+  return new Pool(this, address);
+};
+
+// Instantiate the router
+Sudoswap.prototype.router = async function () {
+  const chainId = (await this.provider.getNetwork()).chainId;
+
+  return new Router(this, chainId);
+};
+
+Sudoswap.prototype.formatDelta = function (val, type) {
+  if (type == "linear") {
+    return ethers.utils.parseUnits(val, "ether").toString();
+  } else {
+    // example val 0.05 = 5%
+    return 1e18 + val * 1e17;
+  }
+};
+
+Sudoswap.prototype.formatFee = function (val) {
+  //example val 0.05 = 5%
+  return ethers.utils.parseUnits(val, "ether").toString();
+};
+
+module.exports = Sudoswap;
